212455f712cf902bcf4082569dd920dcb393ac63
Damien DeVille <damien@ddeville.me>
2016-01-08 12:11:55 +0000

    New IPC project

---
+++ client/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  client
+//
+//  Created by Damien DeVille on 1/8/16.
+//  Copyright © 2016 Damien DeVille. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char **argv)
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ server/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Server
+//
+//  Created by Damien DeVille on 1/8/16.
+//  Copyright © 2016 Damien DeVille. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
